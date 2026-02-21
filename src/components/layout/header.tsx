07a508db2d903795de54ad5744e49ab5e5dd0cad
Makje <markjay.lunas@gmail.com>
2026-02-21 21:23:58 +0800

    feat: Implement a redesigned header with a new layout, rounded corners, and icon-based menu and contact buttons.

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,21 +1,28 @@
+import { Menu02Icon, SentIcon } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
 import { Link } from "@tanstack/react-router";
 import makjeLogoDark from "@/assets/svg/makje-dark.svg";
+import { cn } from "@/lib/utils";
 import { Button, buttonVariants } from "../ui/button";
 
 export default function Header() {
 	return (
-		<header className="sticky top-0 z-50 flex h-16 w-full items-center justify-between bg-primary/10 px-4 backdrop-blur-sm sm:px-6">
+		<header className="sticky top-4 m-4 rounded-full  z-50  h-16 mx-auto max-w-(--breakpoint-sm) bg-primary/10 px-4 backdrop-blur-sm grid grid-cols-3 items-center">
 			<Navigation />
 
-			<div className="flex items-center gap-2">
+			<div className="flex items-center gap-2 justify-self-center">
 				<Link to="/" className="flex items-center space-x-2">
-					<img src={makjeLogoDark} alt="Logo" className="size-10" />
+					<img src={makjeLogoDark} alt="Logo" className="size-8" />
 					<span className="text-xl font-medium tracking-wide">Makje</span>
 				</Link>
 			</div>
 
-			<Link to="/contact" className={buttonVariants()}>
-				Reach Out
+			<Link
+				to="/contact"
+				className={cn(buttonVariants(), "rounded-full w-fit justify-self-end")}
+			>
+				<HugeiconsIcon icon={SentIcon} />
+				<span className="sr-only md:not-sr-only text-xs">Connect</span>
 			</Link>
 		</header>
 	);
@@ -24,7 +31,10 @@ export default function Header() {
 function Navigation() {
 	return (
 		<nav>
-			<Button variant="link">Menu</Button>
+			<Button variant="ghost" size="icon">
+				<HugeiconsIcon icon={Menu02Icon} />
+				<span className="sr-only">Menu</span>
+			</Button>
 		</nav>
 	);
 }
