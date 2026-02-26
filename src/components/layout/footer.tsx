e859bec9a3ea4236e154d1dacfb2771a26437945
Makje <markjay.lunas@gmail.com>
2026-02-26 17:14:12 +0800

    style: Increase Makje logo text size in footer.

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
 					<div className="flex items-center gap-2 justify-self-center">
 						<Link to="/" className="flex items-center space-x-2">
 							<img src={makjeLogoDark} alt="Logo" className="size-8" />
-							<span className="text-xl font-medium tracking-wide">Makje</span>
+							<span className="text-3xl font-medium tracking-wide">Makje</span>
 						</Link>
 					</div>
 
