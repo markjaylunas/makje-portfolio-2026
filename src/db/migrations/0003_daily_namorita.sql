deb9af6bb88e6d35ed476a4b7cc525870760867f
Makje <markjay.lunas@gmail.com>
2026-02-27 15:50:46 +0800

    Rebased Drizzle migrations to a single initial snapshot and added snake_case casing configuration.

--- src/db/migrations/0003_daily_namorita.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "experience_to_technologies" ADD COLUMN "order" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "project_to_tags" ADD COLUMN "order" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "project_to_technologies" ADD COLUMN "order" integer DEFAULT 0 NOT NULL;
