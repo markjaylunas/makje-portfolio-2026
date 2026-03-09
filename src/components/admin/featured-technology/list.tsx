80cb7fda60419d666d558b5fd2d0815a260676e4
Makje <markjay.lunas@gmail.com>
2026-03-09 15:37:43 +0800

    refactor: Move featured technology list title and section wrapper from component to parent route.

--- src/components/admin/featured-technology/list.tsx
+++ src/components/admin/featured-technology/list.tsx
@@ -1,49 +1,42 @@
 import { move } from "@dnd-kit/helpers";
 import { DragDropProvider } from "@dnd-kit/react";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { type ComponentProps, useState } from "react";
+import { useState } from "react";
 import { Column } from "@/components/common/dnd/column";
 import { Item } from "@/components/common/dnd/sortable";
-import H2 from "@/components/common/H2";
 import { TechCardPixel } from "@/components/home/technology/card";
 import { getFeaturedTechnologyListOptions } from "@/data/options/featured-technology";
 
-export default function FeaturedTechnologyCardList(
-	props: ComponentProps<"section">,
-) {
+export default function FeaturedTechnologyCardList() {
 	const { data } = useSuspenseQuery(getFeaturedTechnologyListOptions());
 
 	const [featuredTechnologyList, setFeaturedTechnologyList] = useState(data);
 
 	return (
-		<section {...props}>
-			<H2 className="text-2xl md:text-3xl">Featured Technologies</H2>
-
-			<DragDropProvider
-				onDragOver={(event) => {
-					setFeaturedTechnologyList((v) => move(v, event));
-				}}
-			>
-				<ul className="mx-auto max-w-(--breakpoint-sm) grid grid-cols-2 md:grid-cols-4 mt-6 gap-px bg-muted border border-muted">
-					{Object.entries(featuredTechnologyList).map(
-						([column, { order, id, technology }], index) => (
-							<Column key={column} id={column}>
-								<Item key={id} id={id} index={index} column={column}>
-									<li key={id} className="relative">
-										<p className="absolute z-10 top-2 right-2">{order}</p>
-										<TechCardPixel
-											icon={technology.icon?.url ?? undefined}
-											colors={technology.brandColor}
-											name={technology.name}
-											alt={technology.icon?.altText ?? undefined}
-										/>
-									</li>
-								</Item>
-							</Column>
-						),
-					)}
-				</ul>
-			</DragDropProvider>
-		</section>
+		<DragDropProvider
+			onDragOver={(event) => {
+				setFeaturedTechnologyList((v) => move(v, event));
+			}}
+		>
+			<ul className="mx-auto max-w-(--breakpoint-sm) grid grid-cols-2 md:grid-cols-4 mt-6 gap-px bg-muted border border-muted">
+				{Object.entries(featuredTechnologyList).map(
+					([column, { order, id, technology }], index) => (
+						<Column key={column} id={column}>
+							<Item key={id} id={id} index={index} column={column}>
+								<li key={id} className="relative">
+									<p className="absolute z-10 top-2 right-2">{order}</p>
+									<TechCardPixel
+										icon={technology.icon?.url ?? undefined}
+										colors={technology.brandColor}
+										name={technology.name}
+										alt={technology.icon?.altText ?? undefined}
+									/>
+								</li>
+							</Item>
+						</Column>
+					),
+				)}
+			</ul>
+		</DragDropProvider>
 	);
 }
--- src/routes/_protected/admin/technology/index.tsx
+++ src/routes/_protected/admin/technology/index.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import FeaturedTechnologyCardList from "@/components/admin/featured-technology/list";
 import TechnologyTable from "@/components/admin/technology/table";
 import CreateButton from "@/components/common/create-button";
+import H2 from "@/components/common/H2";
 import { getFeaturedTechnologyListOptions } from "@/data/options/featured-technology";
 import { getTechnologyListOptions } from "@/data/options/technology";
 import { searchSchema } from "@/form-validators/technology";
@@ -29,7 +30,11 @@ function RouteComponent() {
 
 			<TechnologyTable />
 
-			<FeaturedTechnologyCardList className="mt-12" />
+			<section className="mt-12">
+				<H2 className="text-2xl md:text-3xl">Featured Technologies</H2>
+
+				<FeaturedTechnologyCardList />
+			</section>
 		</main>
 	);
 }
