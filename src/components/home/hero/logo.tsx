92ffb4f3c889f13b20f2ef7f8a99c65aa05405f7
Makje <markjay.lunas@gmail.com>
2026-03-16 09:56:43 +0800

    feat: conditionally render LiquidMetalCanvas or a static image based on mobile device detection.

--- src/components/home/hero/logo.tsx
+++ src/components/home/hero/logo.tsx
@@ -2,15 +2,26 @@ import logo from "@/assets/svg/makje.svg";
 import logoDark from "@/assets/svg/makje-dark.svg";
 import { LiquidMetalCanvas } from "@/components/common/liquid-metal";
 import { defaultLiquidMetalParams } from "@/components/common/liquid-metal/params";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function HeroLogo() {
+	const isMobile = useIsMobile();
+
 	return (
 		<div className="mt-14 mb-4 sm:my-12 mx-auto size-28 sm:size-56">
-			<LiquidMetalCanvas
-				imageSrc={logo}
-				params={defaultLiquidMetalParams}
-				imageSrcPlaceholder={logoDark}
-			/>
+			{isMobile ? (
+				<img
+					src={logoDark}
+					alt="Makje logo"
+					className="h-full w-full object-contain"
+				/>
+			) : (
+				<LiquidMetalCanvas
+					imageSrc={logo}
+					params={defaultLiquidMetalParams}
+					imageSrcPlaceholder={logoDark}
+				/>
+			)}
 		</div>
 	);
 }
