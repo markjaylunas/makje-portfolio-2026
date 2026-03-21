ff7b3668a5a957963aa18503221d62994132324b
Makje <markjay.lunas@gmail.com>
2026-03-21 18:25:56 +0800

    feat: enhance contact page with aurora background, large heading, and updated component layout.

--- src/routes/_main/contact.tsx
+++ src/routes/_main/contact.tsx
@@ -8,9 +8,17 @@ export const Route = createFileRoute("/_main/contact")({
 
 function RouteComponent() {
 	return (
-		<main className="max-w-(--breakpoint-lg) mx-auto px-4 sm:px-6 mt-32">
-			<ContactDetails />
-			<ContactForm />
+		<main className="max-w-(--breakpoint-lg) mx-auto px-4 sm:px-6 pt-32 min-h-dvh">
+			<div className="aurora-midnight-background absolute inset-0 z-0 h-[90dvh]">
+				<h1 className="text-7xl md:text-[8rem] lg:text-[10rem] font-bold text-center bg-clip-text text-transparent bg-linear-to-b from-white/10 via-white/5 via-30% to-90% to-transparent uppercase tracking-tight mt-32">
+					Contact
+				</h1>
+			</div>
+
+			<div className="flex flex-col sm:flex-row gap-6 justify-around items-center">
+				<ContactDetails />
+				<ContactForm />
+			</div>
 		</main>
 	);
 }
