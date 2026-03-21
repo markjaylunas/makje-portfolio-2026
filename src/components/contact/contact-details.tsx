9f3b3f917ae77e2980b17cbfad2f6e0af74f9ddd
Makje <markjay.lunas@gmail.com>
2026-03-21 20:31:26 +0800

    feat: implement `Item` UI component and integrate it into `ContactDetails` for displaying social links.

--- src/components/contact/contact-details.tsx
+++ src/components/contact/contact-details.tsx
@@ -1,11 +1,64 @@
+import { ArrowUpRight } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { socialLinks } from "@/lib/constants";
+import {
+	Item,
+	ItemActions,
+	ItemContent,
+	ItemMedia,
+	ItemTitle,
+} from "../ui/item";
+
 export default function ContactDetails() {
 	return (
-		<section className="flex-1 flex flex-col gap-2">
-			<p className="leading-relaxed text-muted-foreground">
-				I&apos;m always open to new opportunities and collaborations. If you
-				have a project in mind or would like to discuss a potential
-				collaboration, feel free to reach out. I&apos;d love to hear from you!
-			</p>
+		<section className="flex-1 flex flex-col gap-4">
+			<div className="flex flex-col gap-2">
+				<h2 className="text-3xl font-bold">Let&apos;s Connect</h2>
+
+				<p className="mt-2 leading-relaxed text-muted-foreground">
+					I&apos;m always open to new opportunities and collaborations. If you
+					have a project in mind or would like to discuss a potential
+					collaboration, feel free to reach out. I&apos;d love to hear from you!
+				</p>
+			</div>
+
+			<div className="flex flex-col gap-2 mt-6">
+				{socialLinks.map((social) => (
+					<a
+						href={social.href}
+						target="_blank"
+						rel="noopener noreferrer"
+						className="rounded-full group"
+						key={social.name}
+					>
+						<Item
+							variant="outline"
+							className="group-hover:bg-accent transition-colors cursor-pointer duration-300 ease-in-out"
+						>
+							<ItemMedia variant="icon">
+								<HugeiconsIcon
+									icon={social.icon}
+									size={20}
+									className="group-hover:text-primary transition-all duration-300 ease-in-out"
+								/>
+							</ItemMedia>
+							<ItemContent>
+								<ItemTitle className="group-hover:text-primary transition-all duration-300 ease-in-out">
+									{social.name}
+								</ItemTitle>
+							</ItemContent>
+
+							<ItemActions>
+								<HugeiconsIcon
+									icon={ArrowUpRight}
+									size={20}
+									className="group-hover:text-primary transition-all duration-300 ease-in-out"
+								/>
+							</ItemActions>
+						</Item>
+					</a>
+				))}
+			</div>
 		</section>
 	);
 }
--- src/routes/_main/contact/index.tsx
+++ src/routes/_main/contact/index.tsx
@@ -9,7 +9,7 @@ export const Route = createFileRoute("/_main/contact/")({
 function RouteComponent() {
 	return (
 		<main className="relative mx-auto max-w-(--breakpoint-lg) px-4 pt-32 sm:px-6">
-			<div className="relative z-10 mt-32 sm:mt-42 flex flex-col items-center sm:items-start justify-around gap-6 sm:flex-row">
+			<div className="relative z-10 mt-32 sm:mt-42 flex flex-col items-center sm:items-start justify-around gap-12 sm:flex-row mb-24">
 				<ContactDetails />
 				<ContactForm />
 			</div>
